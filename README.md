e000dc0237dc34dfeaea2af8ee44e449eaa766b9
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-01-12 15:23:11 +0700

    add docker

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 
 ## Example Page
 ### Sample Page => <a href="https://github.com/MAHAulia/bun-react-js-sample" target="_blank">https://github.com/MAHAulia/bun-react-js-sample</a>
+
+
+## Build Image
+docker build -t sample-react-app .
+
+## Jalankan Container
+docker run -d -p 9090:4173 --name sample-react-app-container sample-react-app
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bun-react-js-sample/',
-  build: {
-    outDir: './docs'
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 9090, // you can replace this port with any port
   }
 })
